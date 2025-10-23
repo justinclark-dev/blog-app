384b056105876114a13cb55eeb5df40bee8f9922
justinclark-dev <justinclark.dev@gmail.com>
2025-10-22 17:38:19 -0700

    adds mongoose connection

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 // server.js
 
 const dotenv = require('dotenv');
-dotenv.config();
+dotenv.config({ quiet: true });
 const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI)
+
+mongoose.connection.on('connected', () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`)
+})
 
 app.get('/', (req, res) => {
     res.send('Yo, it works!');
